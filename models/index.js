d538111e9d3f7297be9337440ccfc30419bcbf2e
Susan Chiemi <Susan@Users-MacBook.local>
2017-08-26 09:45:49 -0400

    new commit to fix heroku connection

--- models/index.js
+++ models/index.js
@@ -3,16 +3,17 @@
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
-// var basename  = path.basename(module.filename);
+var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(path.join(__dirname + '/../config/config.json'))[env];var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+if (config.use_env_variable) {
+var sequelize = new Sequelize(process.env[config.use_env_variable]);
+console.log(sequelize);
+} else {
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 if(process.env.JAWSDB_URL){
   var sequalize=new Sequelize(process.env.JAWSDB_URL);
@@ -41,3 +42,4 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
